9232197b2f58f0a8c803992abb6233342da59891
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-08-28 16:44:19 +0400

    Update index.ts

--- src/routes/general/index.ts
+++ src/routes/general/index.ts
@@ -6,7 +6,7 @@ const data: routesTemplate = {
     route: '/',
     secure: false,
     funct: async (_req: Request, res: Response) => {
-        res.send('This is the API for Otter Bots, will be used for various things in the future! This page will be updated soon.');
+        res.send('This is the web page for the API for Otter Bots (bit confusing). THis will be used for various things in the future. This page will be updated soon.');
     }
 }
 export default data;
